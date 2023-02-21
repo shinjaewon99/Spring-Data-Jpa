fc3047e6e6ac465c34de68c31fc3d01ef0564afe
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-21 11:08:29 +0900

    feat : 초기설정

---
+++ springData-Jpa/src/main/java/study/springDataJpa/SpringDataJpaApplication.java
@@ -0,0 +1,15 @@
+package study.springDataJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+
+public class SpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaApplication.class, args);
+	}
+
+}
