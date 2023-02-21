261b350e3f8ed5980aa2744b80cdc118c9a97584
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-21 11:11:09 +0900

    feat : 순수 JPA 레퍼지토리

---
+++ springData-Jpa/src/main/java/study/springDataJpa/repository/MemberJpaRepository.java
@@ -0,0 +1,60 @@
+package study.springDataJpa.repository;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import study.springDataJpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    public void delete(Member member){
+        em.remove(member);
+
+    }
+
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class, id);
+        // null일수도있고 아닐수도있다.
+        return Optional.ofNullable(member);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+    }
+
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class)
+                // 단 건 조회
+                .getSingleResult();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    // 유저명과 나이조건
+    public List<Member> findByUsernameAndGreaterThen(String username, int age){
+        return em.createQuery("select m from Member m where m.username = :username and m.age > :age")
+                .setParameter("username", username)
+                .setParameter("age", age)
+                .getResultList();
+
+    }
+}
