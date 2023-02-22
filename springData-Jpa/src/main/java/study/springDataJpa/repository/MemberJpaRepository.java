a2b6d7359d21585de4f54bc5f788bfd7b9ff071c
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-22 23:31:07 +0900

    refactor : 순수 JPA의 페이징과 정렬

--- springData-Jpa/src/main/java/study/springDataJpa/repository/MemberJpaRepository.java
+++ springData-Jpa/src/main/java/study/springDataJpa/repository/MemberJpaRepository.java
@@ -64,4 +64,19 @@ public class MemberJpaRepository {
                 .setParameter("username", username)
                 .getResultList();
     }
+
+    public List<Member> findByPage(int age, int offset, int limit){
+       return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+                .setParameter("age", age)
+                // 어디서부터 가져올지 지정
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age){
+        return em.createQuery("select count (m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
--- springData-Jpa/src/test/java/study/springDataJpa/repository/MemberJpaRepositoryTest.java
+++ springData-Jpa/src/test/java/study/springDataJpa/repository/MemberJpaRepositoryTest.java
@@ -98,6 +98,29 @@ class MemberJpaRepositoryTest {
 
     }
 
+    @Test
+    public void paging(){
+        //given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+        //when
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long total = memberJpaRepository.totalCount(age);
+
+
+        //then
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(total).isEqualTo(5);
+
+    }
+
 
 
 
