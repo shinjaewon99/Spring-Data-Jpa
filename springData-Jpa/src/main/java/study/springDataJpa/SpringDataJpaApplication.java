4a3830f49aaadca60b9d1b1faba679f3b4cd87df
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-27 19:27:35 +0900

    refactor : Auditing 어노테이션을 위한 수정사항

--- springData-Jpa/src/main/java/study/springDataJpa/SpringDataJpaApplication.java
+++ springData-Jpa/src/main/java/study/springDataJpa/SpringDataJpaApplication.java
@@ -2,14 +2,37 @@ package study.springDataJpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+import java.util.Optional;
+import java.util.UUID;
 
+@SpringBootApplication
+// 모든 엔티티에 들어가는 공통 컬럼들을 공통Entity를 뽑아내서 사용할때 사용 (대표적으로, 등록일자 수정일자)
+@EnableJpaAuditing
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJpaApplication.class, args);
+	}
+
+
+	@Bean
+	public AuditorAware<String> auditorAware(){
+
+
+		return () -> Optional.of(UUID.randomUUID().toString());
+
+		// 람다식 사용 X
+		/*return new AuditorAware<String>() {
+			@Override
+			public Optional<String> getCurrentAuditor() {
+				return Optional.of(UUID.randomUUID().toString());
+			}
+		};*/
 	}
 
 }
